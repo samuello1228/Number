d34dd9acae8b9133779bbc424af9396ea53e28b9
Samuel Lo <samuel@samuels-macbook-pro-3.local>
2016-02-18 03:50:37 +0800

    hello world

--- PositiveInteger/PositiveInteger.cpp
+++ PositiveInteger/PositiveInteger.cpp
@@ -1,16 +1,9 @@
 //
-//  main.cpp
+//  PositiveInteger.cpp
 //  PositiveInteger
 //
 //  Created by Samuel Lo on 18/2/2016.
 //  Copyright © 2016年 Samuel Lo. All rights reserved.
 //
 
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Hello, World!\n";
-	return 0;
-}
+#include "PositiveInteger.hpp"
---
+++ PositiveInteger/PositiveInteger.hpp
@@ -0,0 +1,17 @@
+//
+//  PositiveInteger.hpp
+//  PositiveInteger
+//
+//  Created by Samuel Lo on 18/2/2016.
+//  Copyright © 2016年 Samuel Lo. All rights reserved.
+//
+
+#ifndef PositiveInteger_hpp
+#define PositiveInteger_hpp
+
+class PositiveInteger
+{
+    
+};
+
+#endif /* PositiveInteger_hpp */
