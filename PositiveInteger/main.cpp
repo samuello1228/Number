5e1637db0e1455b894bb5da0e95b9be954153027
Samuel Lo <samuel@samuels-macbook-pro-3.local>
2016-02-18 03:32:51 +0800

    Initial Commit

---
+++ PositiveInteger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PositiveInteger
+//
+//  Created by Samuel Lo on 18/2/2016.
+//  Copyright © 2016年 Samuel Lo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
